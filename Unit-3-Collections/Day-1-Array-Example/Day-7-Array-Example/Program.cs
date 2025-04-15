8c5d3eab0a5d1f61e9d90239bfb20d26914881e5
Zhane Moorman-Taylor <Zhaneworkmt@gmail.com>
2025-04-15 14:45:47 -0400

    Array and Day 1 Intro modifications April 15, 2025

--- Unit-3-Collections/Day-1-Array-Example/Day-7-Array-Example/Program.cs
+++ Unit-3-Collections/Day-1-Array-Example/Day-7-Array-Example/Program.cs
@@ -25,8 +25,13 @@ class Program
         {
             Console.WriteLine("\nWelcome to my app!");       // Verify the app started
 
+            
+            //const marks the variable as a constant
+            // a constant cannot be changed if it is assigned a value
+            // constant names should be all uppercase with _ to seperate the parts of the name
             // Define a constant to use to reference the size of the array
-            const int ARRAY_SIZE = 5;  // use this every where you want to code the size of the array
+            const int ARRAY_SIZE = 10;  // use this every where you want to code the size of the array
+            // WE USE CONSTANTS TO AVOID ERRORS INVOLVING A CHANGING VARIBALES
 
             // Define an array to hold up to 10 numbers entered by the user
             double[] theNumbers = new double[ARRAY_SIZE];  //define an array of 10 doubles
@@ -39,13 +44,17 @@ class Program
 
             // Define a variable to hold the number of variables entered by the user
             int numberEntered = 0;
+            
 
+            //When you need to process an array from start to end.
+            //Use a for-loop
+            //for(int i=0; i<size-of-array; i++) - use I as the index into the array.
             // Set up a loop to get ARRAY_SIZE numbers, one at a time or responses indicating the user is done
-            for (int i = 0; i < ARRAY_SIZE; i++)
+            for (int i = 0; i < ARRAY_SIZE; i++)    //instead of ARRAY_SIZE theNumbers.Length is OK too. 
             {
                 if (!moreInput()) // if they don't have any more input...
                 {
-                    break;  // exit the for-loop
+                    break;  // exit the for-loop before its gone through all iterations
                 }
                 // At this point we know the user has a number to enter
 
@@ -59,7 +68,7 @@ class Program
             //
             // numberEntered = i;
             //
-            // NO! i is defined in the for loop - so it can be used inside the for-loop (scope)
+            // NO! is defined in the for loop - so it can be used inside the for-loop (scope)
 
             // So now the array has all the numbers entered by the user
 
@@ -69,6 +78,20 @@ class Program
             // Only process the numbers that were entered. i.e. NOT arrayname.length
             for (int i = 0; i < numberEntered; i++)
             {
+              /*  The Ternary Operator: Condition ? value-if-true : value-if-false
+                
+                    theNumbers[i] % 2 == 0 ? "Even" : "Odd"
+                    
+                    % - modulus operator - returns the remainder after an integer divide
+                    Use % when you want to know if something i evenly divisible by something else
+                        6 % 2 - return 0
+                        7 % 2 - return 1 
+                        100 % 33 - return 2 (100 / 33 - 3 with remainder of 2 (99 + 2))
+                        
+                        If you divide a number by 2 and remainder is 0, it's even
+                                                        remainder is 1, it's odd
+                
+                */
                 Console.WriteLine("Element #: " + i + " is: " + theNumbers[i] 
                                 + " it is " + (theNumbers[i] % 2 == 0 ? "Even" : "Odd"));
 
@@ -89,13 +112,16 @@ class Program
          ******************************************************************/
 
         // return a boolean value to indicate if teh user has more input
+        // return true if user entered "Y"
+        //  false 
         static bool moreInput()
         {
+            // if(moreInput != true) //alternate coding style 
             bool   isThereInput  = false;  // Hold teh return value 
 
             string whatUserTyped = "";     // Hold what the user enters
 
-            bool   getInput      = true;   // Control the user interaction loop
+            bool   getInput      = true;   // Control the user interaction loop (indicator)
 
             do
             {
@@ -104,24 +130,29 @@ class Program
                 whatUserTyped = Console.ReadLine();
 
                 whatUserTyped = whatUserTyped.ToUpper();
+                
+                // Convert the user input to all uppercase - so we don't worry about case
 
                 string firstChar = whatUserTyped.Substring(0, 1);
-
+                
+                // Extract the first character from the user input - Substring(start-index, #-of-ychars)
                 if (firstChar == "Y")
                 {
-                    getInput = false;
-                    isThereInput = true;
+                    getInput = false;   //Done getting input -set loop control variable to false
+                    isThereInput = true; //Indicate we have good input - set the indicator to value
                 }
                 else
                 {
                     if (firstChar == "N")
                     {
-                        getInput = false;
-                        isThereInput = false;
+                        getInput = false;   //Done getting input 
+                        isThereInput = false;   // User has no more input 
                     }
                 }
-            } while (getInput); // Loop while we get input
-
+            } while (getInput); // Loop while we get input (while getInput == true)
+            //while(getInput == true)   alternate way of coding 
+            // it's not necessary when you have bool variable since the variable can only be true or false
+            // so the variable name will be true or false no need to == or !=
             return isThereInput;
         }
 
@@ -148,7 +179,20 @@ class Program
 
                 // Get the input from the user
                 string userInput = Console.ReadLine();
-
+                
+                //Some statements may cause an Exception during processing
+                //An exception is a error that occurs when the program runs (aka Runtime Excpetion)
+                //When an Exception occurs a cryptic (to normal people) error message is displayed
+                //      the program stops
+                //
+                // You can handle an exception if it occurs in the program and let the program continue.
+                //  with a try/catch block
+                //
+                // you put code that might cause an Exception ina  try block    try{}
+                // followed by one or more catch blocks for exception
+                //          catch(exception-to-handle name-for-exception-object)
+                //
+                //  The exception object contains information about the exception you might want to use. 
                 try // We want to handle an Exception that might occur in this block of code
                 {
                     // Convert the user input to a double
