dc9dd63e0d8c4d4d65f5aef1c8ef8c4af5e7a6b3
Zhane Moorman-Taylor <Zhaneworkmt@gmail.com>
2025-04-16 18:41:17 -0400

    Ne assignment April 16, 2025

---
+++ Unit-3-Collections/BasicStringProcessing/BasicStringProcessing/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BasicStringProcessing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ok");
+    }
+
+        static string UMMIDK()
+    {
+        /*  QUESTION 1:
+           1)  Prompt the user to enter a sentence.
+           2)  Split the sentence into individual words and display each word on its own line.
+        */
+        Console.WriteLine("----- STARTING PROGRAM -----"); // START OF PROGRAM
+        Console.WriteLine("Please enter a sentence: ");
+        string userResponse = Console.ReadLine();
+        
+        
+        
+    Console.WriteLine("Goodbye!"); //END OF PROGRAM
+    }
+}
